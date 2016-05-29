79e1f7b461ef982c9d5c1f6e62f8e41385ce5470
Paulo Roberto <psface1@gmail.com>
2016-05-28 23:46:08 -0300

    Converte server.js do ES6 para ES5

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-'use strict';
-const config = require('./config/config.js')();
-const app = require('./config/express')();
+var config = require('./config/config.js')();
+var app = require('./config/express')();
 require('./config/passport')();
 require('./config/database')(config.db);
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), function() {
   console.log('Express executado na porta '+app.get('port'));
 });
