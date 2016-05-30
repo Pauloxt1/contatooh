8cb0d365d38bcbe621e5bd48c1bdd22d8c7afb3f
Paulo Roberto <psface1@gmail.com>
2016-05-30 14:29:18 -0300

    Mudando dados da env de production

--- config/env/production.js
+++ config/env/production.js
@@ -1,9 +1,9 @@
 module.exports = {
   env: 'production',
-  db: process.env.OPENSHIFT_MONGO_DB_URL+'contatooh',
+  db: process.env.OPENSHIFT_MONGODB_DB_URL+'contatooh',
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  port: process.env.OPENSHIFT_NODE_JS_PORT,
-  address: process.env.OPENSHIFT_NODE_JS_IP,
+  port: process.env.OPENSHIFT_NODEJS_PORT,
+  address: process.env.OPENSHIFT_NODEJS_IP,
   domain: process.env.OPENSHIFT_APP_DNS
 };
